5fa846f325a57f79f7e366e96560ec87f83ce164
Somnath Prasad <somnathprasad559@gmail.com>
2022-08-19 18:32:10 +0530

    allowed origins update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:8000',
     'http://localhost:3500',
     'https://virtual-hr-be.herokuapp.com',
-    'https://virtual-hr-fe.vercel.app/',
+    'https://virtual-hr-fe.vercel.app',
 ];
 
 module.exports = allowedOrigins;
